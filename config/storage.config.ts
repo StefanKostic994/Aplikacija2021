e080d95af453a58d4b48e353242fd76ea28ad361
StefanKostic994 <stefan.kosstic994@gmail.com>
2021-04-20 16:38:47 +0200

    Implement a mechanism to delete article photos

--- config/storage.config.ts
+++ config/storage.config.ts
@@ -1,6 +1,6 @@
 export const StorageConfig = {
     photo: {
-        destination: '../storage/photos',
+        destination: '../storage/photos/',
         maxSize: 1024 * 1024 * 3,
         resize: {
             thumb: { width: 120, height: 100, directory: 'thumb/' },
