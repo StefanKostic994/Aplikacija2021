f876847c6bfc16a2457d27e5ae1b93e45236edb8
StefanKostic994 <stefan.kosstic994@gmail.com>
2021-04-21 10:58:18 +0200

    Unify user and admin tokens and implement user login

--- src/app.module.ts
+++ src/app.module.ts
@@ -82,7 +82,8 @@ import { UserService } from './services/user/user.service';
     UserService
   ] ,
   exports: [
-    AdministratorService
+    AdministratorService,
+    UserService
   ]      
 })
 export class AppModule implements NestModule {
